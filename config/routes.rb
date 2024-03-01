e95138d600c394034587c5373a153c9a36b2d49f
Isaac Thill <ithill22@turing.edu>
2024-03-01 09:21:15 -0700

    add user controller and testing/serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v0 do
+      resources :users, only: %i[index show create] do
+        resources :leagues, only: %i[index], controller: 'users/leagues'
+      end
+      resources :leagues, only: %i[create show update destroy] do
+        resources :user_leagues, only: %i[index], controller: 'leagues/user_leagues'
+      end
+      resources :user_leagues, only: %i[show create destroy] do
+        resources :draft_picks, only: %i[index], controller: 'user_leagues/roster_teams'
+      end
+      resources :teams, only: %i[index show]
+      resources :draft_picks, only: %i[create]
+      resources :articles, only: %i[index]
+    end
+  end
 end
